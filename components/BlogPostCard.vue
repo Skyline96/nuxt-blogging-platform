fa514bc16ad7b64421b2a50d444bcfbfdc95f36d
Akash Mahadik <akashpm1996@gmail.com>
2025-03-09 14:58:09 +0530

    Create a basic homepage and dyanamically generate details page for each blogpost

---
+++ components/BlogPostCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-4 mb-4 border border-gray-300 rounded">
+    <div class="img-wrapper w-full aspect-[3/2] bg-gray-100 rounded overflow-hidden">
+      <img :src="post.img" alt="">
+    </div>
+    <div class="mt-4">
+      <NuxtLink :to="`/blogs/${post.id}`" class="text-lg font-semibold">{{ post.title }}</NuxtLink>
+      <p class="text-gray-600">{{ post.description }}</p>
+      <span class="inline-block text-sm text-gray-400 mt-2">By {{ post.author }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  post: Object
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    
+    <h1 class="text-3xl font-bold my-4 sm:my-8">Latest Blog Posts</h1>
+    <div class="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4 lg:gap-6">
+      <BlogPostCard v-for="post in blogPosts" :key="post.id" :post="post" />
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-
+<script setup>
+const blogPosts = [
+  { id: 1, title: "First Post", img: "/images/img_placeholder.png", description: "This is my first blog post.", author: "Admin" },
+  { id: 2, title: "Second Post", img: "/images/img_placeholder.png", description: "This is my first blog post.", author: "User A" },
+  { id: 3, title: "First Post", img: "/images/img_placeholder.png", description: "This is my first blog post.", author: "Admin" },
+  { id: 4, title: "Second Post", img: "/images/img_placeholder.png", description: "This is my first blog post.", author: "User A" },
+  { id: 5, title: "First Post", img: "/images/img_placeholder.png", description: "This is my first blog post.", author: "Admin" },
+  { id: 6, title: "Second Post", img: "/images/img_placeholder.png", description: "This is my first blog post.", author: "User A" },
+  { id: 7, title: "First Post", img: "/images/img_placeholder.png", description: "This is my first blog post.", author: "Admin" },
+  { id: 8, title: "Second Post", img: "/images/img_placeholder.png", description: "This is my first blog post.", author: "User A" }
+]
 </script>
-
-<style>
-
-</style>
