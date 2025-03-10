b5258403737e5fd63a773a5d4c57d5a36547bfa5
Akash M. <akashpm1996@gmail.com>
2025-03-10 18:55:05 +0530

    If user is logged in - Show user name and logout option - Hide the login & register page links

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,17 @@
     <header class="bg-blue-600 text-white p-4">
       <nav class="container mx-auto flex justify-between">
         <NuxtLink to="/" class="text-xl font-bold">Blogging Platform</NuxtLink>
-        <div>
+        <div v-if="loggedIn" class="">
+          <NuxtLink to="/" class="px-3">Hi, {{ user.name }}</NuxtLink>
+          <button @click="logout" class="px-3 py-1.5 border border-white rounded">Logout</button>
+        </div>
+        <div v-if="!loggedIn">
           <NuxtLink to="/login" class="px-3">Login</NuxtLink>
           <NuxtLink to="/register" class="px-3">Register</NuxtLink>
         </div>
       </nav>
     </header>
-    <main class="container mx-auto p-4 flex-grow">
+    <main class="container mx-auto px-4">
       <slot />
     </main>
     <footer class="bg-gray-800 text-white p-4 text-center">
@@ -18,8 +22,11 @@
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup>
+const { loggedIn, user, clear: clearSession } = useUserSession()
 
+const logout = async () => {
+  await clearSession()
+  await navigateTo('/login')
+}
 </script>
-
-<style></style>
