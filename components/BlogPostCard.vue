de1667ce72bf8f97cdc3467c8cbfe19201692e50
Akash Mahadik <akashpm1996@gmail.com>
2025-03-11 03:26:05 +0530

    Small changes to 'BlogPostCard' layout

--- components/BlogPostCard.vue
+++ components/BlogPostCard.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="p-4 mb-4 border border-gray-300 rounded">
-    <div class="img-wrapper w-full aspect-[3/2] bg-gray-100 rounded overflow-hidden">
-      <img :src="post.img" alt="">
+  <NuxtLink :to="`/blogposts/${post.id}`">
+    <div class="p-4 mb-4 border border-gray-300 rounded">
+      <div class="img-wrapper w-full aspect-[3/2] bg-gray-100 rounded overflow-hidden">
+        <img src="/images/img_placeholder.png" alt="">
+      </div>
+      <div class="mt-4">
+        <h3 class="text-lg font-semibold">{{ post.title }}</h3>
+        <p class="text-gray-600 line-clamp-3">{{ post.content }}</p>
+        <span class="inline-block text-sm text-gray-400 mt-2">By {{ post.author }}</span>
+      </div>
     </div>
-    <div class="mt-4">
-      <NuxtLink :to="`/blogs/${post.id}`" class="text-lg font-semibold">{{ post.title }}</NuxtLink>
-      <p class="text-gray-600">{{ post.description }}</p>
-      <span class="inline-block text-sm text-gray-400 mt-2">By {{ post.author }}</span>
-    </div>
-  </div>
+  </NuxtLink>
 </template>
 
 <script lang="ts" setup>
