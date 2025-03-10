ecc4ada40353a61013fe3a30c28e2fae67a2fb8f
Akash M. <akashpm1996@gmail.com>
2025-03-10 18:46:19 +0530

    Changes: - Update login api route with registered user credentials and better error handling - Implement server & client side form validations using zod

--- pages/login.vue
+++ pages/login.vue
@@ -1,33 +1,78 @@
 <template>
   <div class="max-w-md mx-auto bg-white p-6">
-    <form @submit.prevent="login">
-      <h2 class="text-2xl font-bold mb-4">Login</h2>
-      <input v-model="credentials.email" type="email" placeholder="Email"
-        class="w-full p-2 border border-gray-300 rounded mb-3" />
-      <input v-model="credentials.password" type="password" placeholder="Password"
-        class="w-full p-2 border border-gray-300 rounded mb-3" />
-      <button type="submit" class="bg-blue-600 text-white w-full p-2 rounded">Login</button>
+    <form @submit.prevent="onSubmit" novalidate>
+      <h2 class="text-3xl font-bold mb-8">Login</h2>
+      <div class="relative mb-8">
+        <input v-model="loginCredentials.email" type="email" placeholder="Email"
+          class="w-full p-2 border border-gray-300 rounded" />
+        <p v-show="loginFormErrors.email" class="absolute inset-x-0 ps-0.5 pt-0.5 text-sm text-red-600">{{
+          loginFormErrors.email }}</p>
+      </div>
+      <div class="relative mb-8">
+        <input v-model="loginCredentials.password" type="password" placeholder="Password"
+          class="w-full p-2 border border-gray-300 rounded" />
+        <p v-show="loginFormErrors.password" class="absolute inset-x-0 ps-1 pt-0.5 text-sm text-red-600">{{
+          loginFormErrors.password }}</p>
+      </div>
+      <div class="relative mb-8">
+        <button type="submit" class="bg-blue-600 text-white w-full p-2 rounded">Login</button>
+        <p v-show="errorMessage" class="absolute inset-x-0 ps-1 pt-0.5 text-sm text-red-600 text-center">{{
+          errorMessage }}</p>
+      </div>
     </form>
   </div>
 </template>
 
 <script setup>
-const { loggedIn, user, fetch: refreshSession } = useUserSession()
-const credentials = reactive({
+const { fetch: refreshSession } = useUserSession()
+const loginCredentials = reactive({
   email: '',
   password: '',
-})
+});
 
-async function login() {
-  $fetch('/api/auth/login', {
-    method: 'POST',
-    body: credentials
-  })
+const loginFormErrors = ref({});
+const errorMessage = ref(null);
+
+const validateLoginFormData = () => {
+  // Parse form data without throwing an error
+  const result = loginFormSchema.safeParse(loginCredentials);
+
+  // Display errors
+  if (!result.success) {
+    loginFormErrors.value = result.error.errors.reduce((acc, error) => {
+      const key = error.path[0];
+      acc[key] = error.message;
+      return acc
+    }, {})
+
+    return false;
+  }
+
+  // Reset errors if data are valid
+  loginFormErrors.value = {};
+  return true;
+};
+
+// Handle form submission
+const onSubmit = async () => {
+  // Reset error message
+  errorMessage.value = '';
+
+  // Check if validation failed
+  if (!validateLoginFormData()) return;
+
+  $fetch('/api/auth/login', { method: 'POST', body: loginCredentials })
     .then(async () => {
+      // Reset input fields
+      loginCredentials.email = ''
+      loginCredentials.password = ''
+
       // Refresh the session on client-side and redirect to the home page
       await refreshSession()
       await navigateTo('/')
     })
-    .catch(() => alert('Bad credentials'))
-}
+    .catch((error) => {
+      errorMessage.value = error.statusMessage;
+    })
+};
 </script>
--- server/api/auth/login.post.js
+++ server/api/auth/login.post.js
@@ -1,25 +1,50 @@
-import { z } from 'zod'
-
-const bodySchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8)
-})
+import bcrypt from "bcrypt"
+import { initDB } from "../../db/database"
 
 export default defineEventHandler(async (event) => {
-    const { email, password } = await readValidatedBody(event, bodySchema.parse)
+    try {
+        const { success, data } = await readValidatedBody(event, body => loginFormSchema.safeParse(body));
 
-    if (email === 'admin@admin.com' && password === 'iamtheadmin') {
-        // set the user session in the cookie
-        // this server util is auto-imported by the auth-utils module
-        await setUserSession(event, {
-            user: {
-                name: 'Admin'
-            }
-        })
-        return {}
+        if (!success) {
+            console.error("Username or password is missing");
+            return createError({
+                statusCode: 400,
+                statusMessage: "Username or password is missing",
+            });
+        }
+
+        // Connect to the database
+        const db = await initDB();
+
+        // Check if the user exist
+        const user = await db.get("SELECT * FROM users WHERE username = ?", [
+            data.email,
+        ]);
+
+        // For security reasons, do not specify if username or password is incorrect
+        if (!user || !(await bcrypt.compare(data.password, user.password))) {
+            console.error(`Invalid username or password`);
+
+            return createError({
+                statusCode: 401,
+                statusMessage: "Invalid username or password",
+            });
+        } else {
+            const userData = { name: user.name, username: user.username };
+
+            await setUserSession(event, {
+                user: userData,
+                loggedInAt: new Date(),
+            });
+        }
+
+        return { success: true, user };
+    } catch (error) {
+        console.error("Error handling login request:", error);
+
+        return createError({
+            statusCode: 500,
+            statusMessage: "Failed to process request",
+        });
     }
-    throw createError({
-        statusCode: 401,
-        message: 'Bad credentials'
-    })
 })
