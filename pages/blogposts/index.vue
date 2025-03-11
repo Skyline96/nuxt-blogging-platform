097ac73cc0fdcb468ba47ca6beae6a00b8afc268
Akash Mahadik <akashpm1996@gmail.com>
2025-03-11 11:06:55 +0530

    Display the authenticated user's blogposts on page '/blogposts' for now

---
+++ pages/blogposts/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold my-4 sm:my-8">My Blog Posts</h1>
+    <div class="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4 lg:gap-6">
+      <BlogPostCard v-for="post in userBlogposts" :key="post.id" :post="post" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data: userBlogposts } = await useFetch(`/api/blogposts/user`)
+</script>
