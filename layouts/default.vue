4770c61b170c8056b0c79ec5d13cb50a63afe5bd
Akash M. <akashpm1996@gmail.com>
2025-03-13 14:04:34 +0530

    Set minimum height to default layout content and update logout button style

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
         <NuxtLink to="/" class="text-xl font-bold">Blogging Platform</NuxtLink>
         <div v-if="loggedIn" class="">
           <NuxtLink to="/dashboard/user-posts" class="px-3">Hi, {{ user.name }}</NuxtLink>
-          <button @click="logout" class="px-3 py-1.5 border border-white rounded">Logout</button>
+          <button @click="logout" class="px-3 py-1.5 bg-white text-blue-600 rounded">Logout</button>
         </div>
         <div v-if="!loggedIn">
           <NuxtLink to="/auth/login" class="px-3">Login</NuxtLink>
@@ -13,11 +13,11 @@
         </div>
       </nav>
     </header>
-    <main class="container mx-auto px-4">
-      <slot />
+    <main class="container mx-auto px-4 min-h-[calc(100vh-116px)]">
+      <slot></slot>
     </main>
     <footer class="bg-gray-800 text-white p-4 text-center">
-      © 2025 My Blog - All Rights Reserved
+      © 2025 Blogging Platform - All Rights Reserved
     </footer>
   </div>
 </template>
